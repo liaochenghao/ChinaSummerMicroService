45ddb68d4a2108f80a4322bf540773e1a6e11f26
QiuLei <896275756@qq.com>
2018-01-16 15:36:27 +0800

    修改微信validators

--- src/api/weixin/validators.py
+++ src/api/weixin/validators.py
@@ -16,19 +16,23 @@ class TemplateMessageValidator(BaseValidator):
         self.validate_func('char', 'openid', self.validate_data)
         self.validate_func('char', 'template_id', self.validate_data)
         self.validate_func('dict', 'send_data', self.validate_data)
+        return self.validate_data
 
 
 class UserInfoValidator(BaseValidator):
     def validate(self):
         self.validate_func('char', 'openid', self.validate_data)
+        return self.validate_data
 
 
 class TemporaryQrCodeValidator(BaseValidator):
     def validate(self):
         self.validate_func('char', 'action_name', self.validate_data)
         self.validate_func('int', 'expired_seconds', self.validate_data)
+        return self.validate_data
 
 
 class ForeverQrCodeValidator(BaseValidator):
     def validate(self):
         self.validate_func('char', 'action_name', self.validate_data)
+        return self.validate_data
